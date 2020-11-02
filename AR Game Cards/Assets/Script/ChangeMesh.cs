184524e2ca1015d359bba61fce41a76ed2459ad3
elisabettapiccolo <56259563+elisabettapiccolo@users.noreply.github.com>
2020-11-02 18:28:29 +0100

    script change mesh

---
+++ AR Game Cards/Assets/Script/ChangeMesh.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       MeshRenderer meshComponent = GetComponent<MeshRenderer>();
+        if (meshComponent != null)
+             {
+            Debug.Log("Trovato!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
