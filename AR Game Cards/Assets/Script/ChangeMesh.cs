80278ea447403f9daaacf76edefa6f2328a1da27
elisabettapiccolo <56259563+elisabettapiccolo@users.noreply.github.com>
2020-11-02 20:16:40 +0100

    changed script

--- AR Game Cards/Assets/Script/ChangeMesh.cs
+++ AR Game Cards/Assets/Script/ChangeMesh.cs
@@ -5,18 +5,35 @@ using UnityEngine;
 public class ChangeMesh : MonoBehaviour
 {
     // Start is called before the first frame update
+       
+    public Mesh Mesh;
+    private Mesh MeshToSwap;
+
+    private MeshFilter Target;
+
+    public float Timer = 3f;
+
+    private float ElapsedTime = 0f;
     void Start()
     {
-       MeshRenderer meshComponent = GetComponent<MeshRenderer>();
-        if (meshComponent != null)
-             {
-            Debug.Log("Trovato!");
-        }
+        Target = GetComponent<MeshFilter>();
+        MeshToSwap = Mesh;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Target == null) return;
+
+        ElapsedTime += Time.deltaTime;
+        if (ElapsedTime >= Timer)
+        {
+            ElapsedTime = 0;
+            Mesh OldMesh = Target.mesh;
+            Target.mesh = MeshToSwap;
+            MeshToSwap = OldMesh;
+        }
     }
 }
+
+    
