2e3f9f5f447d563d16127de7f0dfc38196e464fa
elisabettapiccolo <56259563+elisabettapiccolo@users.noreply.github.com>
2020-11-03 17:05:14 +0100

    added script mastercard

---
+++ AR Game Cards/Assets/Script/MasterCardBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterCardBehaviour : MonoBehaviour
+{
+    public GameObject Barchetta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Barchettacard")
+        {
+            Barchetta.SetActive(true);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.name == "Barchettacard")
+        {
+            Barchetta.SetActive(false);
+
+        }
+
+
+    }
+}
+
+
